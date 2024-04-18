0c8cf8c8d04c3f925207df70c55bcb148b6ffbfd
Annely-Maria Liivas <liivas@ut.ee>
2024-04-18 11:47:07 +0300

    Added corpus data and updated corpus preprocessing

--- data_preprocessing/corpus_methods/file_operations.py
+++ data_preprocessing/corpus_methods/file_operations.py
@@ -1,30 +1,19 @@
-# Different methods for saving and loading corpus data
+# Methods for saving and loading corpus data
 
 # -- imports
 import os
 import json
 from estnltk.converters import text_to_json, json_to_text
 
-# -- new target directory for EstTimeMLCorpus articles in JSON format
-json_dir = 'EstTimeML_corpus_json'
-
-if not os.path.isdir(json_dir):
-    os.mkdir(json_dir)
-
-# -- target folder path
-json_dir_path = 'EstTimeML_corpus_json/'
-
 
 # -- method for converting EstNLTK Text-object to JSON string and saving it in JSON format
-def save_Text_to_json(text_object):
-    #json_text = text_to_json(text_object)
-    #with open(json_dir_path + text_object.meta['filename'] + '.json', 'w') as f:
-    #    json.dump(json_text, f)
-    filename = json_dir_path + text_object.meta['filename'] + '.json'
+def save_Text_to_json(path, text_object):
+    filename = path + text_object.meta['filename'] + '.json'
     text_to_json(text_object, file=filename)
-        
+
+    
 # -- method for loading JSON string from JSON file and returning it as EstNLTK Text-object
-def load_Text_from_json(filename):
-    return json_to_text(file=json_dir_path + filename)
+def load_Text_from_json(path, filename):
+    return json_to_text(file=path + filename)
         
         
